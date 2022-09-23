765f90eb198d0c200c4015c85cb9ae03bad7b40a
Cheomm88 <chopids@gmail.com>
2022-09-23 13:42:41 +0100

    ejemplo

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//cambio random
 public class Enemy : MonoBehaviour
 {
     public void Attack(GameObject projectilPrefab)
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//asddasda
 public class Projectile : MonoBehaviour
 {
     public float speedProjectile = 2f;
